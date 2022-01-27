21c981306f4302703c6df9cc348f545aa2301bd8
p4535992 <tentimarco0@gmail.com>
2022-01-27 13:57:57 +0100

    better calculation for center coordinates

--- src/module/utils.ts
+++ src/module/utils.ts
@@ -1,3 +1,4 @@
+import { TokenData } from '@league-of-foundry-developers/foundry-vtt-types/src/foundry/common/data/module.mjs';
 import { canvas, game } from './settings';
 
 /**
@@ -76,7 +77,7 @@ export function findTokenByName(tokenName: string): Token {
 /**
  * Get token center
  */
-export const getTokenCenter = function (token: Token): { x: number; y: number } {
+export const getTokenCenter = function (token: Token, grid:any = {}): { x: number; y: number } {
   /*
     let tokenCenter = {x: token.x , y: token.y };
     tokenCenter.x += -20 + ( token.w * 0.50 );
@@ -87,9 +88,40 @@ export const getTokenCenter = function (token: Token): { x: number; y: number } 
   //   const shape0 = shapes[0];
   //   return { x: shape0.x, y: shape0.y };
   // }
-  const tokenCenter = { x: token.x + token.w / 2, y: token.y + token.h / 2 };
-  return tokenCenter;
-};
+
+  // const tokenCenter = { x: token.x + token.w / 2, y: token.y + token.h / 2 };
+  // return tokenCenter;
+
+  const data:TokenData = token.data;
+  //getCenter(type, data, grid = {}){
+  let isGridSpace = false;
+  if(token.document.documentName == TileDocument.documentName) {
+    isGridSpace = false;
+  }
+  else if(token.document.documentName == DrawingDocument.documentName){
+    isGridSpace = false;
+  }
+	else 	{
+    isGridSpace = true;
+	}
+  grid = mergeObject({w: canvas.grid?.w, h:canvas.grid?.h}, grid);
+  const [x,y] = [data.x, data.y];
+  let center = {x:x, y:y};
+  //Tokens, Tiles
+  if ( "width" in data && "height" in data ) {
+    let [width, height] = [data.width, data.height];
+    if(isGridSpace){
+      [width, height] = [width * grid.w, height * grid.h];
+    }
+    center={x:x + (Math.abs(width) / 2), y:y + (Math.abs(height) / 2)};
+  }
+  //Walls
+  if("c" in data){
+    //@ts-ignore
+    center = {x:(data.c[0] + data.c[2]) / 2, y: (data.c[1] + data.c[3]) / 2}
+  }
+  return center;
+}
 
 /**
  * Get token shape center
